ce387b5b4082153d60fd81aff36372d32d6fa35f
Jakub Lankiewicz <lankiewiczjakub@gmail.com>
2018-03-27 20:18:39 +0200

    update

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
 var fs = require('fs');
 var colors = require('colors');
 
-fs.readFile('C:\Users\JakubL\Desktop\zadanie_13_7', 'utf-8', function(err, files) {
-    console.log('Dane przed zapisem!'.blue);
-    console.log(files);
-    fs.writeFile('./tekst.txt', 'A tak wyglądają po zapisie!', function(err) {
+fs.readdir('./', function(err, files) {
+    fs.writeFile('./tekst.txt', files, function(err) {
         if (err) throw err;
         console.log('Zapisano!'.blue);
-        fs.readFile('./tekst.txt', 'utf-8', function(err, data) {
-            console.log('Dane po zapisie'.blue)
-            console.log(data);
-        });
     });
 });
